9cd7dbff10eea32b224078b1d6490e62387c25af
tasueom <tasueom@naver.com>
2026-07-18 20:54:20 +0900

    feat(backend): add user entity with auditing timestamps

---
+++ backend/src/main/java/com/careerlog/user/entity/User.java
@@ -0,0 +1,27 @@
+package com.careerlog.user.entity;
+
+import com.careerlog.global.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String passwordHash;
+
+    @Column(nullable = false, length = 50)
+    private String nickname;
+}
